d83785470a5065a98785c567422c79907d507aec
Tan Minh <m.tan2007@gmail.com>
2016-04-25 23:55:45 +0700

    add things

---
+++ Assets/RectTransformExtension.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class RectTransformExtension {
+
+	public static Rect GetScreenRect(this RectTransform rectTransform, Canvas canvas) {
+
+		Vector3[] corners = new Vector3[4];
+		Vector3[] screenCorners = new Vector3[2];
+
+		rectTransform.GetWorldCorners(corners);
+
+		if (canvas.renderMode == RenderMode.ScreenSpaceCamera || canvas.renderMode == RenderMode.WorldSpace)
+		{
+			screenCorners[0] = RectTransformUtility.WorldToScreenPoint(canvas.worldCamera, corners[1]);
+			screenCorners[1] = RectTransformUtility.WorldToScreenPoint(canvas.worldCamera, corners[3]);
+		}
+		else
+		{
+			screenCorners[0] = RectTransformUtility.WorldToScreenPoint(null, corners[1]);
+			screenCorners[1] = RectTransformUtility.WorldToScreenPoint(null, corners[3]);
+		}
+
+		screenCorners[0].y = Screen.height - screenCorners[0].y;
+		screenCorners[1].y = Screen.height - screenCorners[1].y;
+
+		return new Rect(screenCorners[0], screenCorners[1] - screenCorners[0]);
+	}
+
+	public static Vector2 GetScreenWorldPos(this RectTransform rectTran){
+		Vector3[] corners = new Vector3[4];
+		rectTran.GetWorldCorners (corners);
+
+//		foreach(Vector3 v in corners){
+//			Debug.Log (v);
+//		}
+
+		Vector2 pos;
+
+		pos.x = (corners [0].x + corners [3].x) / 2;
+		pos.y = (corners [0].y + corners [1].y) / 2;
+			
+		return pos;
+	}
+
+}
---
+++ Assets/SteeringWheel.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteeringWheel : MonoBehaviour {
+
+	Vector2 rectWordPos;
+	float initAngle;
+	float dragAngle;
+	RectTransform rectTran;
+	public float angleThreshold = 45f;
+
+	// Use this for initialization
+	void OnEnable () {
+		Lean.LeanTouch.OnFingerDown += OnFingerDown;
+		Lean.LeanTouch.OnFingerUp += OnFingerUp;
+		Lean.LeanTouch.OnFingerDrag += OnFingerDrag;
+	}
+
+	void Start(){
+		rectTran = GetComponent<RectTransform> ();
+		rectWordPos = RectTransformExtension.GetScreenWorldPos (rectTran);
+	}
+	
+	void OnFingerDown(Lean.LeanFinger fg){
+		initAngle = fg.GetDegrees (rectWordPos);
+		dragAngle = initAngle;
+	}
+
+	void OnFingerDrag(Lean.LeanFinger fg){
+		dragAngle += fg.GetDeltaDegrees(rectWordPos);
+		float angleDiff = (dragAngle - initAngle) * -1;
+		Debug.Log (angleDiff);
+
+		if(Mathf.Abs(angleDiff) < angleThreshold){
+			rectTran.Rotate (Vector3.forward, fg.GetDeltaDegrees(rectWordPos));
+		}
+	}
+
+	void OnFingerUp(Lean.LeanFinger fg){
+		//reset rotation
+	}
+}
